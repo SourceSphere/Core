0196b3802e4007891baa5c8500040626b3246657
Guilherme Dio <ggrdio@gmail.com>
2013-02-09 15:28:51 -0200

    Handle de arquivo

---
+++ Core/src/main/java/br/com/sourcesphere/core/arquivo/Arquivo.java
@@ -0,0 +1,69 @@
+package br.com.sourcesphere.core.arquivo;
+
+import java.util.Calendar;
+
+/**
+ * Classe para armazenar dados exclusivos de um arquivo
+ * @author Guilherme Dio
+ * @since 1.0
+ *
+ */
+public class Arquivo
+{
+	private String nome;
+	private String extensao;
+	private long tamanho;
+	private String caminhoReal;
+	private String caminhoWeb;
+	private Calendar ultimaModificacao;
+	
+	public String getNome() 
+	{
+		return nome;
+	}
+	public String getExtensao() 
+	{
+		return extensao;
+	}
+	public long getTamanho() 
+	{
+		return tamanho;
+	}
+	public String getCaminhoReal()
+	{
+		return caminhoReal;
+	}
+	public String getCaminhoWeb()
+	{
+		return caminhoWeb;
+	}
+	public Calendar getUltimaModificacao() 
+	{
+		return ultimaModificacao;
+	}
+	public void setNome(String nome)
+	{
+		this.nome = nome;
+	}
+	public void setExtensao(String extensao) 
+	{
+		this.extensao = extensao;
+	}
+	public void setTamanho(long tamanho)
+	{
+		this.tamanho = tamanho;
+	}
+	public void setCaminhoReal(String caminhoReal) 
+	{
+		this.caminhoReal = caminhoReal;
+	}
+	public void setCaminhoWeb(String caminhoWeb)
+	{
+		this.caminhoWeb = caminhoWeb;
+	}
+	public void setUltimaModificacao(Calendar ultimaModificacao)
+	{
+		this.ultimaModificacao = ultimaModificacao;
+	}
+
+}
